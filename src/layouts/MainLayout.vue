9695888551b38d15d35adee8c88c06b25afa2fc9
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2026-04-19 09:38:11 -0400

    novo layouyt

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,27 +1,25 @@
 <template>
   <q-layout view="hHh lpR fFf" class="bg-grey-1">
-    <q-header elevated class="bg-white text-grey-8" height-hint="64">
-      <q-toolbar class="GNL__toolbar">
-        <q-btn
-          flat
-          dense
-          round
-          @click="toggleLeftDrawer"
-          aria-label="Menu"
-          icon="menu"
-          class="q-mr-sm"
-        />
+    <q-header elevated class="bg-white text-grey-8 q-py-xs" height-hint="58">
+      <q-toolbar>
+        <q-btn flat dense round @click="toggleLeftDrawer" aria-label="Menu" icon="menu" />
 
-        <q-toolbar-title v-if="$q.screen.gt.xs" shrink class="row items-center no-wrap">
-          <img src="https://cdn.quasar.dev/img/layout-gallery/logo-google.svg" />
-          <span class="q-ml-sm">News</span>
-        </q-toolbar-title>
+        <q-btn flat no-caps no-wrap class="q-ml-xs" v-if="$q.screen.gt.xs">
+          <q-icon :name="fabYoutube" color="red" size="28px" />
+          <q-toolbar-title shrink class="text-weight-bold"> YouTube </q-toolbar-title>
+        </q-btn>
 
         <q-space />
 
         <div class="q-gutter-sm row items-center no-wrap">
-          <q-btn v-if="$q.screen.gt.sm" round dense flat color="text-grey-7" icon="apps">
-            <q-tooltip>Google Apps</q-tooltip>
+          <q-btn round dense flat color="grey-8" icon="video_call" v-if="$q.screen.gt.sm">
+            <q-tooltip>Create a video or post</q-tooltip>
+          </q-btn>
+          <q-btn round dense flat color="grey-8" icon="apps" v-if="$q.screen.gt.sm">
+            <q-tooltip>Apps</q-tooltip>
+          </q-btn>
+          <q-btn round dense flat color="grey-8" icon="message" v-if="$q.screen.gt.sm">
+            <q-tooltip>Messages</q-tooltip>
           </q-btn>
           <q-btn round dense flat color="grey-8" icon="notifications">
             <q-badge color="red" text-color="white" floating> 2 </q-badge>
@@ -37,70 +35,79 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered class="bg-white" :width="280">
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered class="bg-grey-2" :width="240">
       <q-scroll-area class="fit">
-        <q-list padding class="text-grey-8">
-          <q-item
-            class="GNL__drawer-item"
-            v-ripple
-            v-for="link in links1"
-            :key="link.text"
-            clickable
-          >
+        <q-list padding>
+          <q-item v-for="link in links1" :key="link.text" v-ripple clickable>
             <q-item-section avatar>
-              <q-icon :name="link.icon" />
+              <q-icon color="grey" :name="link.icon" />
             </q-item-section>
             <q-item-section>
               <q-item-label>{{ link.text }}</q-item-label>
             </q-item-section>
           </q-item>
 
-          <q-separator inset class="q-my-sm" />
+          <q-separator class="q-my-md" />
 
-          <q-item
-            class="GNL__drawer-item"
-            v-ripple
-            v-for="link in links2"
-            :key="link.text"
-            clickable
-          >
+          <q-item v-for="link in links2" :key="link.text" v-ripple clickable>
             <q-item-section avatar>
-              <q-icon :name="link.icon" />
+              <q-icon color="grey" :name="link.icon" />
             </q-item-section>
             <q-item-section>
               <q-item-label>{{ link.text }}</q-item-label>
             </q-item-section>
           </q-item>
 
-          <q-separator inset class="q-my-sm" />
+          <q-separator class="q-mt-md q-mb-xs" />
 
-          <q-item
-            class="GNL__drawer-item"
-            v-ripple
-            v-for="link in links3"
-            :key="link.text"
-            clickable
-          >
+          <q-item-label header class="text-weight-bold text-uppercase">
+            More from Youtube
+          </q-item-label>
+
+          <q-item v-for="link in links3" :key="link.text" v-ripple clickable>
+            <q-item-section avatar>
+              <q-icon color="grey" :name="link.icon" />
+            </q-item-section>
             <q-item-section>
-              <q-item-label
-                >{{ link.text }} <q-icon v-if="link.icon" :name="link.icon"
-              /></q-item-label>
+              <q-item-label>{{ link.text }}</q-item-label>
             </q-item-section>
           </q-item>
 
-          <div class="q-mt-md">
-            <div class="flex flex-center q-gutter-xs">
-              <a class="GNL__drawer-footer-link" href="javascript:void(0)" aria-label="Privacy"
-                >Privacy</a
+          <q-separator class="q-my-md" />
+
+          <q-item v-for="link in links4" :key="link.text" v-ripple clickable>
+            <q-item-section avatar>
+              <q-icon color="grey" :name="link.icon" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>{{ link.text }}</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-separator class="q-mt-md q-mb-lg" />
+
+          <div class="q-px-md text-grey-9">
+            <div class="row items-center q-gutter-x-sm q-gutter-y-xs">
+              <a
+                v-for="button in buttons1"
+                :key="button.text"
+                class="YL__drawer-footer-link"
+                href="javascript:void(0)"
               >
-              <span> · </span>
-              <a class="GNL__drawer-footer-link" href="javascript:void(0)" aria-label="Terms"
-                >Terms</a
+                {{ button.text }}
+              </a>
+            </div>
+          </div>
+          <div class="q-py-md q-px-md text-grey-9">
+            <div class="row items-center q-gutter-x-sm q-gutter-y-xs">
+              <a
+                v-for="button in buttons2"
+                :key="button.text"
+                class="YL__drawer-footer-link"
+                href="javascript:void(0)"
               >
-              <span> · </span>
-              <a class="GNL__drawer-footer-link" href="javascript:void(0)" aria-label="About"
-                >About Google</a
-              >
+                {{ button.text }}
+              </a>
             </div>
           </div>
         </q-list>
@@ -115,108 +122,84 @@
 
 <script>
 import { ref } from 'vue'
-import { fasEarthAmericas, fasFlask } from '@quasar/extras/fontawesome-v6'
+import { fabYoutube } from '@quasar/extras/fontawesome-v6'
 
 export default {
-  name: 'GoogleNewsLayout',
+  name: 'MyLayout',
 
   setup() {
     const leftDrawerOpen = ref(false)
     const search = ref('')
-    const showAdvanced = ref(false)
-    const showDateOptions = ref(false)
-    const exactPhrase = ref('')
-    const hasWords = ref('')
-    const excludeWords = ref('')
-    const byWebsite = ref('')
-    const byDate = ref('Any time')
-
-    function onClear() {
-      exactPhrase.value = ''
-      hasWords.value = ''
-      excludeWords.value = ''
-      byWebsite.value = ''
-      byDate.value = 'Any time'
-    }
-
-    function changeDate(option) {
-      byDate.value = option
-      showDateOptions.value = false
-    }
 
     function toggleLeftDrawer() {
       leftDrawerOpen.value = !leftDrawerOpen.value
     }
 
     return {
+      fabYoutube,
+
       leftDrawerOpen,
       search,
-      showAdvanced,
-      showDateOptions,
-      exactPhrase,
-      hasWords,
-      excludeWords,
-      byWebsite,
-      byDate,
+
+      toggleLeftDrawer,
 
       links1: [
-        { icon: 'web', text: 'Top stories' },
-        { icon: 'person', text: 'For you' },
-        { icon: 'star_border', text: 'Favourites' },
-        { icon: 'search', text: 'Saved searches' },
+        { icon: 'home', text: 'Home' },
+        { icon: 'whatshot', text: 'Trending' },
+        { icon: 'subscriptions', text: 'Subscriptions' },
       ],
       links2: [
-        { icon: 'flag', text: 'Canada' },
-        { icon: fasEarthAmericas, text: 'World' },
-        { icon: 'place', text: 'Local' },
-        { icon: 'domain', text: 'Business' },
-        { icon: 'memory', text: 'Technology' },
-        { icon: 'local_movies', text: 'Entertainment' },
-        { icon: 'directions_bike', text: 'Sports' },
-        { icon: fasFlask, text: 'Science' },
-        { icon: 'fitness_center', text: 'Health ' },
+        { icon: 'folder', text: 'Library' },
+        { icon: 'restore', text: 'History' },
+        { icon: 'watch_later', text: 'Watch later' },
+        { icon: 'thumb_up_alt', text: 'Liked videos' },
       ],
       links3: [
-        { icon: '', text: 'Language & region' },
-        { icon: '', text: 'Settings' },
-        { icon: 'open_in_new', text: 'Get the Android app' },
-        { icon: 'open_in_new', text: 'Get the iOS app' },
-        { icon: '', text: 'Send feedback' },
-        { icon: 'open_in_new', text: 'Help' },
+        { icon: fabYoutube, text: 'YouTube Premium' },
+        { icon: 'local_movies', text: 'Movies & Shows' },
+        { icon: 'videogame_asset', text: 'Gaming' },
+        { icon: 'live_tv', text: 'Live' },
       ],
-
-      onClear,
-      changeDate,
-      toggleLeftDrawer,
+      links4: [
+        { icon: 'settings', text: 'Settings' },
+        { icon: 'flag', text: 'Report history' },
+        { icon: 'help', text: 'Help' },
+        { icon: 'feedback', text: 'Send feedback' },
+      ],
+      buttons1: [
+        { text: 'About' },
+        { text: 'Press' },
+        { text: 'Copyright' },
+        { text: 'Contact us' },
+        { text: 'Creators' },
+        { text: 'Advertise' },
+        { text: 'Developers' },
+      ],
+      buttons2: [
+        { text: 'Terms' },
+        { text: 'Privacy' },
+        { text: 'Policy & Safety' },
+        { text: 'Test new features' },
+      ],
     }
   },
 }
 </script>
 
 <style lang="sass">
-.GNL
+.YL
 
-  &__toolbar
-    height: 64px
-
-  &__toolbar-input
+  &__toolbar-input-container
+    min-width: 100px
     width: 55%
 
-  &__drawer-item
-    line-height: 24px
-    border-radius: 0 24px 24px 0
-    margin-right: 12px
-
-    .q-item__section--avatar
-      .q-icon
-        color: #5f6368
-
-    .q-item__label
-      color: #3c4043
-      letter-spacing: .01785714em
-      font-size: .875rem
-      font-weight: 500
-      line-height: 1.25rem
+  &__toolbar-input-btn
+    border-radius: 0
+    border-style: solid
+    border-width: 1px 1px 1px 0
+    border-color: rgba(0,0,0,.24)
+    max-width: 60px
+    width: 100%
 
   &__drawer-footer-link
     color: inherit
